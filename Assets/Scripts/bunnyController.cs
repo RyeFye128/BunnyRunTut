4d27e37134da508fe484060290e5bb37d1814a8f
Ryan Chartier <ryefye@outlook.com>
2016-05-14 17:39:02 -0400

    Initial Commit

---
+++ Assets/Scripts/bunnyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bunnyController : MonoBehaviour {
+
+	private Rigidbody2D bunny;
+	public float bunnyJumpForce = 500.0f;
+	// Use this for initialization
+	void Start () {
+		bunny = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonUp ("Jump")) 
+		{
+			bunny.AddForce (transform.up*bunnyJumpForce);
+		}
+	}
+}
